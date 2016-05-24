0247261e5cc9084c5da23399634f3792e77cc2f5
jdar22169 <mmow313@gmail.com>
2016-05-24 10:04:53 -0700

    server is working, need to work on testing

--- Maddie/server.js
+++ Maddie/server.js
@@ -1,51 +1,54 @@
 'use strict'
 const fs = require('fs');
+const stream = require('stream');
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const jsonParser = bodyParser.jsonParser
-const fistRouter = express.Router();
+const jsonParser = bodyParser.json();
+const firstRouter = express.Router();
 
-//app.use(jsonParser);
+app.use(jsonParser);
+
 firstRouter.post('/', (req,res) => {
   console.log('hit post route');
-  let frenchie;
-  res.on('data', (data) => {
+  let frenchie = '';
+  req.on('data', (data) => {
     frenchie += data.toString();
+
   })
-  res.end('end', () => {
+  req.on('end', () => {
     var nextFile = (fs.readdirSync(__dirname + '/data/')).length +1
     let file = fs.createWriteStream(__dirname + '/data/frenchie' + nextFile + '.json')
     var bufferStream = new stream.PassThrough();
     let inBuf = new Buffer(frenchie);
     bufferStream.end(inBuf);
     bufferStream.pipe(file);
-    res.send('Saved a Frenchie')
+    res.json({message: 'Saved a Frenchie'})
   })
 })
 
 firstRouter.get('/', (req,res) => {
   console.log('hit get route');
-  res.send('Hello, here is a Frenchie')
+  res.send('Hello, here is a Frenchie' + '\n')
 })
 
 
 firstRouter.put('/:frenchie_id', (req,res) => {
   console.log('hit the put route');
-    let frenchie_id = req.params.frenchie_id
-    var stream = fs.createWriteStream(__dirname + 'data/frenchie' + frenchie_id + '.json')
+    let id = req.params.frenchie_id
+    var stream = fs.createWriteStream(__dirname + '/data/frenchie' + id + '.json')
     req.pipe(stream);
-    res.send('Updated Frenchie')
+    res.send('Updated Frenchie' + '\n')
   })
-})
+
 
 firstRouter.delete('/:frenchie_id', (req,res) => {
   console.log('hit the delete route');
-  let frenchie_id = req.params.frenchie_id
-  fs.unlinkSync(__dirname + 'data/frenchie' + frenchie_id + '.json')
-  res.send('deleted frenchie')
+  var frenchie_id = req.params.frenchie_id
+  fs.unlinkSync(__dirname + '/data/frenchie' + frenchie_id + '.json')
+  res.send('deleted frenchie' + '\n')
 })
 
 
-//app.use('/frenchies', firstRouter);
+app.use('/frenchies', firstRouter);
 app.listen(3000, () => console.log('listening on 3000'));
