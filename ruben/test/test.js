b74f9039a691f404538f63eabd03ca4440cceb2d
Ruben Sarkisyan <rubens24@uw.edu>
2016-05-24 13:10:29 -0700

    wrote tests

--- ruben/test/test.js
+++ ruben/test/test.js
@@ -2,21 +2,25 @@
 const chai = require('chai');
 const expect = chai.expect;
 const chaiHTTP = require('chai-http');
-chai.use('chaiHTTP');
+chai.use(chaiHTTP);
 require('../server');
 
 describe('express tests', () => {
   it('should hit all routes successfully', (done) => {
     chai.request('localhost:3000')
      .get('/players')
-     .post('/post')
-     .put('/put')
      .end(function(err, res){
-     res.should.have.status(404);
+     expect(res).to.have.status(200);
      done();
   });
-//   it('should successfully delete')
-//   .delete('/:id')
-//   .delete('/delete')
-});
+  it('should successfully delete', () => {
+    .delete('/delete')
+  });
+  it('should successfully post', () => {
+    .post('/post')
+  });
+  it('should successfully put' () => {
+    .put('/put')
+  });
+  });
 });
