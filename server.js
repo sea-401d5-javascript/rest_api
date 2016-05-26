ebbb76fa1f760ee3b9e50003d2ca3bf383652fef
YOUR FULL NAME <YOUR EMAIL ADDRESS>
2016-05-26 10:23:18 -0700

    deleleted consts in server

--- server.js
+++ server.js
@@ -6,19 +6,12 @@ const bodyParser = require('body-parser');
 const jsonParser = bodyParser.json();
 const Rooney = require('./schema/rooney');
 const rooneyRouter = express.Router();
-const fs = require('fs');
-const stream = require('stream');
 const mongoose = require('mongoose');
 const morgan = require('morgan');
 
 app.use(morgan('dev'));
 
 mongoose.connect('mongodb://localhost/dev_db');
-
-const dir =  __dirname + '/../data';
-if (!fs.existsSync(dir)) {
-  fs.mkdirSync(dir);
-}
 
 rooneyRouter.use(jsonParser);
 
@@ -56,16 +49,10 @@ rooneyRouter.delete('/:id', jsonParser, (req, res) => {
     if(err) return res.json({message: err.message});
     res.send('deleted Rooney with id ' + req.params.id);
   });
-  // console.log('delete route hit');
-  // let id = req.params.id;
-  // fs.unlinkSync(__dirname + `/../data/${id}.json`)
-  // res.send(`File ${id}.json successfully deleted`)
 });
 
 app.get('/*', (req, res) => {
-  res.status(404).json({msg: 'not found'})
-})
-
-
+  res.status(404).json({msg: 'not found'});
+});
 
 app.listen(3000, () => console.log('up on 3000'));
