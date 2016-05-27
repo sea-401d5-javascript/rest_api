073ab8754f05c72374c0812b9a341faa7a31697c
YOUR FULL NAME <YOUR EMAIL ADDRESS>
2016-05-26 22:41:48 -0700

    finished deleting old commented out code in server and started tests

--- server.js
+++ server.js
@@ -5,11 +5,7 @@ const app = express();
 const mongoose = require('mongoose');
 const morgan = require('morgan');
 const errorHandler =require('./lib/error_handling');
-// const bodyParser = require('body-parser').json();
-// const ManUnitedPlayer = require('./schema/man_United_Player');
-// const BarcaPlayer = require('./schema/barca_Player');
-// const manUnitedRouter = express.Router();
-// const barcaRouter = express.Router();
+
 const dbPort = process.env.MONGOLAB_URI || 'mongodb://localhost/dev_db';
 
 app.use(morgan('dev'));
@@ -20,108 +16,9 @@ const manUnitedRouter = require('./routes/man_united_routes');
 const barcaRouter = require('./routes/barca_routes');
 const compareRouter = require('./routes/compare_route');
 
-// manUnitedRouter.use(bodyParser);
-// barcaRouter.use(bodyParser);
-
 app.use('/manUnited', manUnitedRouter);
 app.use('/barca', barcaRouter);
 app.use('/compare', compareRouter);
-
-// manUnitedRouter.get('/', (req, res) => {
-//   ManUnitedPlayer.find({}, (err,data) => {
-//     if(err) return res.json({
-//       message: err.message
-//     });
-//     res.json(data);
-//   });
-// });
-//
-// manUnitedRouter.post('/', bodyParser,  (req, res) => {
-//   let newManUnitedPlayer = new ManUnitedPlayer(req.body);
-//   newManUnitedPlayer.save((err, data) => {
-//     if(err) return res.json({
-//       message: err.message
-//     });
-//     res.json(data);
-//   });
-// });
-//
-// manUnitedRouter.put('/', bodyParser, (req, res) => {
-//   ManUnitedPlayer.findOneAndUpdate({_id: req.body._id}, req.body, (err,data) => {
-//     if(err) return res.json({message: err.message});
-//     res.json(data);
-//   });
-// });
-//
-// manUnitedRouter.delete('/:id', bodyParser, (req, res) => {
-//   let _id = req.params.id;
-//   ManUnitedPlayer.findOneAndRemove({_id}, null, (err,data) => {
-//     if(err) return res.json({message: err.message});
-//     res.send('deleted Man United Player with id ' + req.params.id);
-//   });
-// });
-
-// barcaRouter.get('/', (req, res) => {
-//   BarcaPlayer.find({}, (err, data) => {
-//     if(err) return res.json({
-//       message: err.message
-//     });
-//     res.json(data);
-//   });
-// });
-//
-// barcaRouter.post('/', bodyParser, (req, res) => {
-//   let newBarcaPlayer = new BarcaPlayer(req.body);
-//   newBarcaPlayer.save((err, data) => {
-//     if(err) return res.json({message: err.message});
-//     res.json(data);
-//   });
-// });
-//
-// barcaRouter.put('/', bodyParser, (req, res) => {
-//   BarcaPlayer.findOneAndUpdate({_id: req.body._id}, req.body, (err, data) => {
-//     if(err) return res.json({message: err.message});
-//     res.json(data);
-//   });
-// });
-//
-// barcaRouter.delete('/:id', bodyParser, (req, res) => {
-//   let _id = req.params.id;
-//   BarcaPlayer.findOneAndRemove({_id}, null, (err,data) => {
-//     if(err) return res.json({message: err.message});
-//     res.send('deleted Barca Player with id ' + req.params.id);
-//   });
-// });
-
-// manUnitedRouter.get('/mostGoals', (req, res, next) => {
-//     let teamGoalArray = [];
-//
-//   ManUnitedPlayer.find({}, (err, player)=>{
-//     if (err) return next(err);
-//     let totalManUGoalsScored = player.reduce((acc, player) => {
-//       return acc += player.goals;
-//     }, 0);
-//     teamGoalArray.push(totalManUGoalsScored);
-//     console.log(teamGoalArray);
-//   });
-//   BarcaPlayer.find({}, (err, player)=>{
-//     if (err) return next(err);
-//     let totalBarcaGoalsScored = player.reduce((acc, player) => {
-//       return acc += player.goals;
-//     }, 0);
-//     console.log(totalBarcaGoalsScored);
-//     teamGoalArray.push(totalBarcaGoalsScored);
-//
-//     if (teamGoalArray[1] > teamGoalArray[0]) {
-//       res.json("Barcalona players scored " + teamGoalArray[1] + " goals, which is more than Man United Players.")
-//     } else if (teamGoalArray[1] < teamGoalArray[0]) {
-//       res.json("Man United players scored " + teamGoalArray[0] + " goals, which is more than Barcalona Players.")
-//     } else {
-//       res.json("They scored the same amount.")
-//     }
-//   });
-//
-// });
 
 app.use((err, req, res, next) => {
   res.status(500).json({message: err.message});
--- test/rest_api_test.js
+++ test/rest_api_test.js
@@ -2,96 +2,11 @@
 
 const chai = require('chai');
 const chaiHTTP = require('chai-http');
-const expect = chai.expect;
-chai.use(chaiHTTP);
-const request = chai.request;
+
+
+
 const fs = require('fs');
+chai.use(chaiHTTP);
 
-let fileArr = [];
-let newFileArr = [];
-let newDeletedFileArr = [];
-let newFileOne;
-let fileOne;
-
-
-
-require(__dirname + '/../server');
-
-describe('rest api tests', () => {
-  before('read files in data', (done) => {
-    fileArr = fs.readdirSync(__dirname + '/../data');
-    done();
-     });
-   it('should post correctly', (done) => {
-    request('localhost:3000')
-      .post('/rooney')
-      .send('{"test": "test"}')
-      .end((err, res) => {
-        newDeletedFileArr = fs.readdirSync(__dirname + '/../data');
-        expect(err).to.eql(null);
-        expect(newDeletedFileArr).to.eql(fileArr);
-        expect(res).to.have.status(200);
-        done();
-      });
-  });
-  it('should delete correctly', (done) => {
-   request('localhost:3000')
-     .delete('/rooney/8')
-     .end((err, res) => {
-       newFileArr = fs.readdirSync(__dirname + '/../data');
-       expect(err).to.eql(null);
-       expect(newFileArr).to.not.eql(fileArr);
-       expect(res).to.have.status(200);
-       done();
-     });
- });
-  it('should respond to a failed test', (done) => {
-    request('localhost:3000')
-    .get('/fail')
-    .end((err, res) => {
-      expect(err).to.not.eql(null);
-      expect(res).to.have.status(404);
-      done();
-    });
-  });
-  it('should process get request correctly', (done) => {
-    request('localhost:3000')
-      .get('/rooney')
-      .end((err, res) => {
-        expect(err).to.eql(null);
-        expect(res).to.have.status(200);
-        expect(res.text).to.eql('GLORY MAN UNITED');
-        done();
-      });
-  });
-
-  it('should turn special id get request to uppercase', (done) => {
-    request('localhost:3000')
-    .get('/rooney/wayne')
-    .end((err, res) => {
-      expect(err).to.eql(null);
-      expect(res).to.have.status(200);
-      expect(res.text).to.eql('{"message":"WAYNE"}');
-      done();
-    });
-  });
-
-describe('put tests', () => {
-  before('read file', () => {
-     fileOne = fs.readFileSync(__dirname + '/../data/1.json').toString();
-  })
-  it('should put properly', (done) =>
-   {
-    request('localhost:3000')
-      .put('/rooney/1')
-      .send('{"test": "test"}')
-      .end((err, res) => {
-        newFileOne = fs.readFileSync(__dirname + '/../data/1.json').toString();
-        expect(err).to.eql(null);
-        expect(newFileOne).to.not.eql(fileOne);
-        expect(res).to.have.status(200);
-        done();
-      });
-  });
-})
-})
+const expect = chai.expect;
+const request = chai.request;
