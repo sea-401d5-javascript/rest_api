9e5fdac6f9abf3257e68a4d4cdb1afef297e828a
YOUR FULL NAME <YOUR EMAIL ADDRESS>
2016-05-26 22:44:50 -0700

    declared consts in test

--- test/rest_api_test.js
+++ test/rest_api_test.js
@@ -2,11 +2,13 @@
 
 const chai = require('chai');
 const chaiHTTP = require('chai-http');
-
-
-
-const fs = require('fs');
+const ManUnitedPlayer = require('../schema/man_United_Player');
+const BarcaPlayer = require('../schema/barca_Player');
+const mongoose = require('mongoose');
 chai.use(chaiHTTP);
 
 const expect = chai.expect;
 const request = chai.request;
+const dbPort = process.env.MONGOLAB_URI;
+process.env.MONGOLAB_URI = 'mongodb://localhost/test_db';
+require('../server');
