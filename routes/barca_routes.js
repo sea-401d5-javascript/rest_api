7003eaf8ed50c6ac54d6c5cceb7a8a6d3b5d237d
YOUR FULL NAME <YOUR EMAIL ADDRESS>
2016-05-26 23:32:14 -0700

    wrote next err for barca routes

--- routes/barca_routes.js
+++ routes/barca_routes.js
@@ -6,34 +6,35 @@ const BarcaPlayer = require('../schema/barca_Player');
 
 const router = module.exports = exports = express.Router();
 
-router.get('/', (req, res) => {
+router.get('/', (req, res, next) => {
   BarcaPlayer.find({}, (err, data) => {
-    if(err) return res.json({
-      message: err.message
-    });
+    if(err) return next(err);
     res.json(data);
   });
 });
 
-router.post('/', bodyParser, (req, res) => {
+router.post('/', bodyParser, (req, res, next) => {
   let newBarcaPlayer = new BarcaPlayer(req.body);
   newBarcaPlayer.save((err, data) => {
-    if(err) return res.json({message: err.message});
+    if(err) return next(err);
     res.json(data);
   });
 });
 
-router.put('/', bodyParser, (req, res) => {
-  BarcaPlayer.findOneAndUpdate({_id: req.body._id}, req.body, (err, data) => {
-    if(err) return res.json({message: err.message});
-    res.json(data);
+router.put('/', bodyParser, (req, res, next) => {
+  let _id = req.body.id;
+  BarcaPlayer.findOneAndUpdate({_id}, req.body, (err, data) => {
+    if(err) return next(err);
+    let message = 'successfully updated';
+    res.json({message});
   });
 });
 
-router.delete('/:id', bodyParser, (req, res) => {
+router.delete('/:id', bodyParser, (req, res, next) => {
   let _id = req.params.id;
   BarcaPlayer.findOneAndRemove({_id}, null, (err,data) => {
-    if(err) return res.json({message: err.message});
-    res.send('deleted Barca Player with id ' + req.params.id);
+    if(err) return next(err);
+    let message = 'successfully deleted';
+    res.json({message});
   });
 });
