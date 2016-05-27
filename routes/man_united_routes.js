effcbae4737d4a5d957d53ad0a7f36e4711a2e76
YOUR FULL NAME <YOUR EMAIL ADDRESS>
2016-05-26 23:29:03 -0700

    wrote next err for man u routes

--- routes/man_united_routes.js
+++ routes/man_united_routes.js
@@ -6,20 +6,18 @@ const ManUnitedPlayer = require('../schema/man_United_Player');
 
 const router = module.exports = exports = express.Router();
 
-router.get('/', (req, res) => {
+router.get('/', (req, res, next) => {
   ManUnitedPlayer.find({}, (err,data) => {
-    if(err) return res.json({
-      message: err.message
+    if(err) return nex(err);
     });
     res.json(data);
   });
 });
 
-router.post('/', bodyParser,  (req, res) => {
+router.post('/', bodyParser,  (req, res, next) => {
   let newManUnitedPlayer = new ManUnitedPlayer(req.body);
   newManUnitedPlayer.save((err, data) => {
-    if(err) return res.json({
-      message: err.message
+    if(err) return nex(err);
     });
     res.json(data);
   });
@@ -34,10 +32,11 @@ router.put('/', bodyParser, (req, res, next) => {
   });
 });
 
-router.delete('/:id', bodyParser, (req, res) => {
+router.delete('/:id', bodyParser, (req, res, next) => {
   let _id = req.params.id;
   ManUnitedPlayer.findOneAndRemove({_id}, null, (err,data) => {
-    if(err) return res.json({message: err.message});
-    res.send('deleted Man United Player with id ' + req.params.id);
+    if(err) return next(err);
+    let message = 'successfully deleted';
+    res.json({message});
   });
 });
