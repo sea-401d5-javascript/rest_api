a975d4169f88ba9fa1b2eb17794ace48962a207d
Chris Perez <chris.k.perez@gmail.com>
2016-05-27 16:59:50 -0700

    dealt with async

--- ChrisPerez/lib/server.js
+++ ChrisPerez/lib/server.js
@@ -4,12 +4,14 @@ const express = require('express');
 const mongoose = require('mongoose');
 const snakeRouter = require(__dirname + '/routes/snakeRouter.js');
 const weaselRouter = require(__dirname + '/routes/weaselRouter.js');
+const battleRouter = require(__dirname + '/routes/battleRouter.js');
 const app = express();
 
 mongoose.connect('mongodb://localhost/dev_db');
 
 app.use('/snakes', snakeRouter);
 app.use('/weasels', weaselRouter);
+app.use('/battle', battleRouter);
 
 app.get('/*', (req, res)=>{
   res.status(404).json({message:'not found'});
