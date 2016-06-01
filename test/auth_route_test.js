502e66dad9d9b3c80b12052548b67bf6be2cd90f
YOUR FULL NAME <YOUR EMAIL ADDRESS>
2016-06-01 10:51:04 -0700

    declared neccessary consts for auth_test

--- test/auth_route_test.js
+++ test/auth_route_test.js
@@ -2,4 +2,12 @@
 
 const chai = require('chai');
 const chaiHTTP = require('chai-http');
-const User = require('../')
+const User = require('../model/user');
+const mongoose = require('mongoose');
+chai.use(chaiHTTP);
+
+const expect = chai.expect;
+const request = chai.request;
+const dbPort = process.env.MONGOLAB_URI;
+process.env.MONGOLAB_URI = 'mongodb://localhost/test_db';
+require('../server');
