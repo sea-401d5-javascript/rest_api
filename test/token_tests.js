2c393c540bd39db56b75a34ad28e2e4d358b11e5
YOUR FULL NAME <YOUR EMAIL ADDRESS>
2016-06-02 10:26:56 -0700

    token test file set up ready to write tests

---
+++ test/token_tests.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const chai = require('chai');
+const chaiHTTP = require('chai-http');
+const User = require('../model/user');
+const mongoose = require('mongoose');
+chai.use(chaiHTTP);
+
+const expect = chai.expect;
+const request = chai.request;
+const dbPort = process.env.MONGOLAB_URI;
+process.env.MONGOLAB_URI = 'mongodb://localhost/test_db';
+require('../server');
+
+describe('Token tests', () => {
+
+  after((done) => {
+    process.env.MONGOLAB_URI = dbPort;
+    mongoose.connection.db.dropDatabase(() => {
+      done();
+    });
+  });
+});
