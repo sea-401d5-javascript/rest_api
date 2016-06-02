bc0b9111cfa7173e69551310a8ced5c8836049f2
YOUR FULL NAME <YOUR EMAIL ADDRESS>
2016-06-02 11:01:38 -0700

    working on tests

--- test/auth_route_test.js
+++ test/auth_route_test.js
@@ -5,43 +5,60 @@ const chaiHTTP = require('chai-http');
 const User = require('../model/user');
 const mongoose = require('mongoose');
 chai.use(chaiHTTP);
+const jwt = require('jsonwebtoken');
 
 const expect = chai.expect;
 const request = chai.request;
+
+const secret = process.env.SECRET || 'changeme';
 const dbPort = process.env.MONGOLAB_URI;
 process.env.MONGOLAB_URI = 'mongodb://localhost/test_db';
 require('../server');
 
 describe('Authorization router tests', () => {
 
-  after((done) => {
-    process.env.MONGOLAB_URI = dbPort;
-    mongoose.connection.db.dropDatabase(() => {
-      done();
-    });
-  });
-  describe('Authorization tests', () => {
-    it('Should sign up a new user', (done) => {
-      request('localhost:6969')
-      .post('/auth/signup')
-      .send({username: 'testy', password: 'test'})
-      .end((err, res) => {
-        expect(err).to.eql(null);
-        expect(res).to.have.status(200);
-        expect(res.body.token).to.eql('token');
+
+    let testUser;
+    beforeEach((done) => {
+      let newUser = new User({
+        username: 'testuser',
+        password: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1NzUwNzAzNWNjZDYzYmY5MGE1ZWZjYjQiLCJpYXQiOjE0NjQ4ODkzOTd9.eExJviSNGYMwcGpnVArhzUFw-eIXmNrdiUzY-0AwNTY'
+      });
+      newUser.save((err, user) => {
+        testUser = user;
         done();
       });
     });
+
+    after((done) => {
+      process.env.MONGOLAB_URI = dbPort;
+      mongoose.connection.db.dropDatabase(() => {
+        done();
+      });
+    });
+
+    // it('Should sign up a new user', (done) => {
+    //   request('localhost:6969')
+    //   .post('/auth/signup')
+    //   .send({username: 'testy', password: 'test'})
+    //   .end((err, res) => {
+    //     expect(err).to.eql(null);
+    //     expect(res).to.have.status(200);
+    //     expect(res.body.token).to.eql(User);
+    //     done();
+    //   });
+    // });
     it('Should login a user', (done) => {
       request('localhost:6969')
       .get('/auth/login')
-      .auth('testy', 'test')
+      .auth('testuser', 'testuser')
       .end((err, res) => {
         expect(err).to.eql(null);
         expect(res).to.have.status(200);
-        expect(res.body.token).to.eql('token');
+        expect(res.body.token).to.eql(jwt.sign({
+          _id: testUser._id
+        }, secret));
         done();
       });
     });
-  });
 });
