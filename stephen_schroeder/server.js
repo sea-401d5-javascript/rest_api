640defa5bac1c0f8fe7f2df9e720a7d8d2bf3064
Stephen Schroeder <schrode50@gmail.com>
2016-06-02 22:36:02 -0700

    auth testing

--- stephen_schroeder/server.js
+++ stephen_schroeder/server.js
@@ -1,8 +1,10 @@
 'use strict';
 
-const express = require('express');
-const app = module.exports = exports = express();
+const app = module.exports = exports = require('express')();
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser').json();
+const jwtAuth = require('/lib/jwt_auth');
+
 const errorHandle = require(__dirname + '/lib/err_handler');
 
 const dbPort = process.env.MONGOLAB_URI || 'mongodb://localhost/marvel_app_dev';
@@ -12,8 +14,17 @@ mongoose.connect(dbPort);
 const marvelRouter = require(__dirname + '/routes/marvel_routes');
 const dcRouter = require(__dirname + '/routes/dc_routes');
 const duelRouter = require(__dirname + '/routes/duel_routes');
+const authRouter = require(__dirname + '/routes/auth_routes');
 
-app.use('/api', marvelRouter, dcRouter, duelRouter);
+app.use('/api', marvelRouter, dcRouter, duelRouter, authRouter);
+
+app.get('/test', (req, res) => {
+  res.send('don\'t need a token');
+});
+
+app.post('/test', bodyParser, jwtAuth, (req, res) => {
+  res.send({ message: 'need a token' });
+});
 
 app.use(errorHandle);
 
