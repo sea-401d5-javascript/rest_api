7bddb66e20cb6ccb74ae82831631ce8454f0b575
David Park <dpark4657@gmail.com>
2016-06-03 12:27:22 -0700

    deleted maddie

--- Maddie/schema/dogWalkers.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-const mongoose = require('mongoose');
-const DogWalker = new mongoose.Schema({
-  name: String,
-  dogs_died: Number
-});
-
-module.exports = mongoose.model('dogWalker', DogWalker);
--- Maddie/schema/frenchies.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-const mongoose = require('mongoose');
-const Frenchie = new mongoose.Schema({
-  name: String,
-  dogWalkers_bitten: Number
-});
-
-module.exports = mongoose.model('frenchie', Frenchie);
