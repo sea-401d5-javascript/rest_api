5e7aab25f4e3644f8a114b0acef79fed4fc97eb7
Dan Stineback <dstineback@gmail.com>
2016-06-20 12:36:53 -0700

    commented out auth for class

--- dan-stineback/server.js
+++ dan-stineback/server.js
@@ -4,18 +4,21 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const morgan = require('morgan');
+const cors = require('cors');
+
 
 const dbPort = process.env.MONGODB_URI || 'mongodb://localhost/dev_db';
 mongoose.connect(dbPort);
 
 const dogRouter = require(__dirname + '/routes/dog-route');
 const catRouter = require(__dirname + '/routes/cat-route');
-const authRouter = require(__dirname + '/routes/router');
+// const authRouter = require(__dirname + '/routes/router');
 
+app.use(cors());
 app.use(morgan('dev'));
 app.use('/dogs', dogRouter);
 app.use('/cats', catRouter);
-app.use('/', authRouter);
+// app.use('/', authRouter);
 
 app.use((err, req, res, next) => {
   res.status(500).json({message: err.message});
